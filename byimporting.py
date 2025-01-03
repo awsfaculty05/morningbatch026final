a6c213bc3e6bbc2dd53865eaf7d6bba6655e78d7
awsfaculty05 <awsfaculty05@fusion-institute.com>
2025-01-03 09:00:33 +0530

    Add files via upload

---
+++ byimporting.py
@@ -0,0 +1,13 @@
+import addition
+import math
+import csv
+import random
+
+x = input('Enter first number: ')
+y = input('Enter second number: ')
+print(addition.add(x,y))
+print(math.factorial(9))
+print(random.sample(range(1,25),20))
+
+
+
